358c99c3bb7eb91f1e1d04720c59e44853906c27
rbokel <rbokel.github@qlod.org>
2019-09-10 17:42:36 +0200

    volumebar

--- src/ts/components/hugereplaybutton.ts
+++ src/ts/components/hugereplaybutton.ts
@@ -1,28 +1,50 @@
-import {ButtonConfig, Button} from './button';
-import {DOM} from '../dom';
-import {UIInstanceManager} from '../uimanager';
-import { PlayerAPI } from 'bitmovin-player';
+import { ButtonConfig, Button } from "./button";
+import { DOM } from "../dom";
+import { UIInstanceManager } from "../uimanager";
+import { PlayerAPI } from "bitmovin-player";
 
 /**
  * A button to play/replay a video.
  */
 export class HugeReplayButton extends Button<ButtonConfig> {
-
   constructor(config: ButtonConfig = {}) {
     super(config);
 
-    this.config = this.mergeConfig(config, {
-      cssClass: 'ui-hugereplaybutton',
-      text: 'Replay',
-    }, this.config);
+    this.config = this.mergeConfig(
+      config,
+      {
+        cssClass: "ui-hugereplaybutton",
+        text: "Replay"
+      },
+      this.config
+    );
   }
 
   configure(player: PlayerAPI, uimanager: UIInstanceManager): void {
     super.configure(player, uimanager);
 
     this.onClick.subscribe(() => {
-      player.play('ui');
+      this.getDomElement().dispatchSmashcutPlayerUiEvent({
+        action: "play",
+        originator: "HugeReplayButton"
+      });
+      player.play("ui");
     });
+
+    if (this.hasTooltip()) {
+      const tooltip = this.getTooltip();
+
+      this.getDomElement().on("mouseleave", () => {
+        tooltip.setText("", 0, 0, false);
+      });
+
+      this.getDomElement().on("mouseover", e => {
+        const target = e.target as HTMLTextAreaElement;
+        const left = target.offsetLeft - target.offsetWidth;
+        const top = target.offsetTop;
+        tooltip.setText("Replay", left, top, false);
+      });
+    }
   }
 
   protected toDomElement(): DOM {
@@ -32,9 +54,11 @@ export class HugeReplayButton extends Button<ButtonConfig> {
     // Setting the image directly on the button does not work together with scaling animations, because the button
     // can cover the whole video player are and scaling would extend it beyond. By adding an inner element, confined
     // to the size if the image, it can scale inside the player without overshooting.
-    buttonElement.append(new DOM('div', {
-      'class': this.prefixCss('image'),
-    }));
+    buttonElement.append(
+      new DOM("div", {
+        class: this.prefixCss("image")
+      })
+    );
 
     return buttonElement;
   }
